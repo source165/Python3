29729e34a76b6b599d9e432571ff6bf40fe3bf23
Source165 <33445579+source165@users.noreply.github.com>
2019-03-16 15:17:19 +0530

    First ever program on python3

---
+++ first.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+#simple first program in python
+
+print ("Hello world!")
+print ("""Hey welcome to the world of python
+We can do much more with python....please stich with me!""")
